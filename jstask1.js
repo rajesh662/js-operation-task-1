df13b738b339fc62439e2ff0c97908b122da471c
rajesh662 <rajkuttyrajesh56@gmail.com>
2023-01-31 16:36:37 +0530

    upload

---
+++ jstask1.js
@@ -0,0 +1,133 @@
+/*1.Find Addition*/
+var a = 40;
+var b = 20;
+var n= a + b;
+document.write("addition <br>");
+document.write(n );
+document.write("<br>");
+
+/*2.Calculate Subtraction*/
+var a = 20;
+var b = 30;
+var n= a-b;
+document.write("subtrsction<br> ");
+document.write(n);
+document.write("<br>");
+
+
+/*3.Calculate Multiplication */
+var a = 20;
+var b = 30;
+var n= a*b;
+document.write("multiplication<br> ");
+document.write(n);
+document.write("<br> ");
+
+/*4.Calculate Division*/
+var a = 30;
+var b = 2;
+var n= a/b;
+document.write("division<br> ");
+document.write(n);
+document.write("<br> ");
+
+
+/*5. Program to calculate Area of Circle */
+var r=2;
+var area=22/7;
+var n=area*r**2;
+document.write("area cricle<br> ");
+document.write(n);
+document.write("<br> ");
+
+/*6.Program to calculate Area of  Triangle*/
+var h=20;
+var b=30;
+var n=1/2*h*b;
+document.write("area of triangle<br> ");
+document.write(n);
+document.write("<br> ");
+
+/*7.Program for Calculating Simple Interest*/
+var p=200;
+var r=2;
+var t=5;
+var n=(p*r*t/100);
+document.write("simple interset<br> ");
+document.write(n);
+document.write("<br> ");
+
+/*8.Program for calculating Square */
+var s=2;
+var n=s**2;
+document.write("square<br> ");
+document.write(n);
+document.write("<br> ");
+
+/*9.Program for calculating Cube*/
+var c=3;
+var n=c**3;
+document.write("cube<br> ");
+document.write(n);
+document.write("<br> ");
+/*10.Program to calculate total and average*/
+
+
+var num1=20;
+var num2=30;
+var num3=40;
+var num4=50;
+var totalaverage=(num1+num2+num3+num4);
+document.write("total<br> ");
+document.write(totalaverage);
+document.write("<br> ");
+document.write("total average<br> ");
+document.write(totalaverage/4);
+document.write("<br> ");
+
+/*11.Check number is prime or not*/
+
+var prime=3;
+
+if(prime>1){
+	document.write("prime number");
+	document.write("<br> ");
+}
+else{
+	document.write("prime not");
+	document.write("<br> ");
+}
+/*12.Program for larger number*/
+
+var larger=20;
+
+var n=larger<19;
+document.write("larger number<br> ");
+document.write(n);
+document.write("<br> ");
+/*13.Program to check number is positive or not*/
+
+var positive=1;
+
+var n=positive>0;
+document.write("positive or not<br> ");
+document.write(n);
+document.write("<br> ");
+
+/*14.Program to check number is greater than 10*/
+	
+var greater=11;
+
+var n=greater>10;
+
+document.write("greater than 10<br> ");
+document.write(n);
+document.write("<br> ");
+/*15.Number even or odd*/
+
+var a=10;
+
+var n=a%2==0;
+document.write("even or add<br> ");
+document.write(n);
+document.write("<br> ");
